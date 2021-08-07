70d2d254863dc151358e27729dce23b5c18cbe14
atomu21263 <atom.aizu@gmail.com>
2021-08-07 20:36:49 +0900

    辞書の追加

--- go.mod
+++ go.mod
@@ -1,10 +1,10 @@
-module github.com/takanakahiko/discord-tts
+module github.com/atomu21263/atomic_bot
 
 go 1.15
 
 require (
 	github.com/bwmarrin/discordgo v0.22.0
 	github.com/jonas747/dca v0.0.0-20201113050843-65838623978b
-	github.com/jonas747/ogg v0.0.0-20161220051205-b4f6f4cf3757 // indirect
+	github.com/jonas747/ogg v0.0.0-20161220051205-b4f6f4cf3757
 	golang.org/x/text v0.3.6
 )
--- main.go
+++ main.go
@@ -1,42 +1,75 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"io"
+	"net/url"
+	"time"
+
 	"log"
 	"os"
 	"os/signal"
+	"regexp"
 	"strconv"
 	"strings"
+	"sync"
 	"syscall"
 
 	"github.com/bwmarrin/discordgo"
-	"github.com/takanakahiko/discord-tts/session"
+	"github.com/jonas747/dca"
+	"golang.org/x/text/language"
 )
 
+type SessionData struct {
+	guildID      string
+	channelID    string
+	vcsession    *discordgo.VoiceConnection
+	speech_speed float64
+	speech_limit int
+	speech_lang  string
+	mut          sync.Mutex
+}
+
+func GetByGuildID(guildID string) (*SessionData, error) {
+	for _, s := range sessions {
+		if s.guildID == guildID {
+			return s, nil
+		}
+	}
+	return nil, fmt.Errorf("Cant find GuildID")
+}
+
 var (
-	sessionManager = session.NewTtsSessionManager()
-	prefix         = flag.String("prefix", "", "call prefix")
-	token          = flag.String("token", "", "bot token")
-	clientID       = ""
+	//変数定義
+	prefix   = flag.String("prefix", "", "call prefix")
+	token    = flag.String("token", "", "bot token")
+	clientID = ""
+	sessions = []*SessionData{}
 )
 
 func main() {
+	//flag入手
 	flag.Parse()
 	fmt.Println("prefix       :", *prefix)
 	fmt.Println("token        :", *token)
 
+	//bot起動準備
 	discord, err := discordgo.New()
 	if err != nil {
-		fmt.Println("Error logging in")
-		fmt.Println(err)
+		fmt.Println("Error logging")
 	}
 
+	//token入手
 	discord.Token = "Bot " + *token
+
+	//eventトリガー設定
 	discord.AddHandler(onReady)
 	discord.AddHandler(onMessageCreate)
 	discord.AddHandler(onVoiceStateUpdate)
 
+	//起動
 	if err = discord.Open(); err != nil {
 		fmt.Println(err)
 	}
@@ -45,195 +78,482 @@ func main() {
 			log.Println(err)
 		}
 	}()
-
+	//起動メッセージ表示
 	fmt.Println("Listening...")
 
+	//bot停止対策
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
 	<-sc
 }
 
+//BOTの準備が終わったときにCall
 func onReady(discord *discordgo.Session, r *discordgo.Ready) {
 	clientID = discord.State.User.ID
-	discord.UpdateStatus(0,*prefix+" help")
+	servers := 0
+	for _, _ = range discord.State.Guilds {
+		servers++
+	}
+	discord.UpdateStatus(0, *prefix+" help | "+strconv.Itoa(servers)+"個の鯖で稼働中")
 }
 
-//event by message
+//メッセージが送られたときにCall
 func onMessageCreate(discord *discordgo.Session, m *discordgo.MessageCreate) {
+	//一時変数
+	GuildID := m.GuildID
+	Guild_tmp, _ := discord.Guild(GuildID)
+	Guild := Guild_tmp.Name
+	ChannelID := m.ChannelID
+	Channel, _ := discord.Channel(ChannelID)
+	Message := m.ID
+	Content := m.Content
+	Author := m.Author.Username
+	AuthorID := m.Author.ID
 
-
-	discordChannel, err := discord.Channel(m.ChannelID)
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-
-	guild, err := discord.Guild(m.GuildID)
-	if err != nil && err != session.ErrTtsSessionNotFound {
-		log.Println(err)
-		return
-	}
-	//logのやつ
-	log.Println("server:\""+guild.Name+"\"    ch:"+discordChannel.Name+"    user:"+m.Author.Username+"    message:"+m.Content)
+	//表示
+	log.Print("Guild:\"" + Guild + "\"  Channel:\"" + Channel.Name + "\"  " + Author + ": " + strings.Replace(Content, "\n", " \\n ", -1))
 
 	//bot 読み上げ無し のチェック
 	if m.Author.Bot || strings.HasPrefix(m.Content, ";") {
 		return
 	}
 
-	if PrefixCheck(m.Content, "help") {
-		discord.ChannelMessageSend(m.ChannelID, "\n使用可能コマンド:\n"+*prefix+" join : VCに接続します\n"+*prefix+" speed <speech speed> : 読み上げ速度を変更します\n"+*prefix+" lang <language code> : 読み上げ言語を変更します\n"+*prefix+" limit <speech limit> : 読み上げ文字数の上限を設定します\n"+*prefix+" leave : VCから切断します")
-		return
-	}
-
-	// "join" command
-	if PrefixCheck(m.Content, "join") {
-		_, err := sessionManager.GetByGuidID(m.GuildID)
-		if err != nil && err != session.ErrTtsSessionNotFound {
-			log.Println(err)
-			return
-		}
-		if err == nil {
-			sendMessage(discord, m.ChannelID, "Bot is already in voice-chat.")
-			return
-		}
-		ttsSession := session.NewTtsSession()
-		if err := ttsSession.Join(discord, m.Author.ID, m.ChannelID); err != nil {
-			if err := discord.MessageReactionAdd(m.ChannelID,m.ID,"❌"); err!= nil {
-				log.Println(err)
-			}	
-			log.Println(err)
-			return
-		}
-		if err = sessionManager.Add(ttsSession); err != nil {
-			log.Println(err)
-		}
-		if err := discord.MessageReactionAdd(m.ChannelID,m.ID,"✅"); err!= nil {
-			log.Println(err)
-		}
-		return
-	}
-
-	// ignore case of "not join" or "include ignore prefix"
-	ttsSession, err := sessionManager.GetByGuidID(m.GuildID)
-	if err == session.ErrTtsSessionNotFound {
-		return
-	}
-	if err != nil {
-		log.Println(err)
-		return
-	}
-
-	// Ignore if the TextChanelID of session and the channel of the message are different
-	if ttsSession.TextChanelID != m.ChannelID {
-		return
-	}
-
-	// other commands
 	switch {
-	case PrefixCheck(m.Content, "leave"):
-		if err := ttsSession.Leave(discord); err != nil {
-			log.Println(err)
-		}
-		if err := sessionManager.Remove(ttsSession.GuidID()); err != nil {
-			log.Println(err)
-		}
-		if err := discord.MessageReactionAdd(m.ChannelID,m.ID,"⛔"); err!= nil {
-			log.Println(err)
-		}
-		return
-	case PrefixCheck(m.Content, "speed"):
-		speedStr := strings.Replace(m.Content, *prefix+" speed ", "", 1)
-		newSpeed, err := strconv.ParseFloat(speedStr, 64)
-		if err != nil {
-			ttsSession.SendMessage(discord, "数字ではない値は設定できません")
-			return
-		}
-		if err = ttsSession.SetSpeechSpeed(discord, newSpeed); err != nil {
-			log.Println(err)
-			return
-		}
-		if err := discord.MessageReactionAdd(m.ChannelID,m.ID,"🔊"); err!= nil {
-			log.Println(err)
-		}
-		return
-	case PrefixCheck(m.Content, "lang"):
-		newLang := strings.Replace(m.Content, *prefix+" lang ", "", 1)
-		if err = ttsSession.SetLanguage(discord, newLang); err != nil {
-			if err := discord.MessageReactionAdd(m.ChannelID,m.ID,"❌"); err!= nil {
+	//TTS関連
+	case Prefix(Content, "jjoin"):
+		if _, err := GetByGuildID(GuildID); err == nil {
+			if err := discord.MessageReactionAdd(ChannelID, Message, "❌"); err != nil {
 				log.Println(err)
-			}	
-			log.Println(err)
+			}
 			return
 		}
-		if err := discord.MessageReactionAdd(m.ChannelID,m.ID,"🗣️"); err!= nil {
-			log.Println(err)
-		}
+		Join(ChannelID, GuildID, discord, AuthorID, Message)
 		return
-	case PrefixCheck(m.Content, "limit"):
-		LimitStr := strings.Replace(m.Content, *prefix+" limit ", "", 1)
-		newLimit, err := strconv.Atoi(LimitStr)
-		if err != nil {
-			ttsSession.SendMessage(discord, "数字ではない値は設定できません")
-			return
-		}
-		if err = ttsSession.SetSpeechLimit(discord, newLimit); err != nil {
-			if err := discord.MessageReactionAdd(m.ChannelID,m.ID,"❌"); err!= nil {
+	case Prefix(Content, "sspeed "):
+		session, err := GetByGuildID(GuildID)
+		if err != nil || session.channelID != ChannelID {
+			if err := discord.MessageReactionAdd(ChannelID, Message, "❌"); err != nil {
 				log.Println(err)
-			}	
-			log.Println(err)
+			}
 			return
 		}
-		if err := discord.MessageReactionAdd(m.ChannelID,m.ID,"🥺"); err!= nil {
-			log.Println(err)
+		Speed(session, Content, discord, ChannelID, Message)
+		return
+	case Prefix(Content, "llang "):
+		session, err := GetByGuildID(GuildID)
+		if err != nil || session.channelID != ChannelID {
+			if err := discord.MessageReactionAdd(ChannelID, Message, "❌"); err != nil {
+				log.Println(err)
+			}
+			return
 		}
+		Lang(session, Content, discord, ChannelID, Message)
+		return
+	case Prefix(Content, "llimit "):
+		session, err := GetByGuildID(GuildID)
+		if err != nil || session.channelID != ChannelID {
+			if err := discord.MessageReactionAdd(ChannelID, Message, "❌"); err != nil {
+				log.Println(err)
+			}
+			return
+		}
+		Limit(session, Content, discord, ChannelID, Message)
+		return
+	case Prefix(Content, "wword "):
+		session, err := GetByGuildID(GuildID)
+		if err != nil || session.channelID != ChannelID {
+			if err := discord.MessageReactionAdd(ChannelID, Message, "❌"); err != nil {
+				log.Println(err)
+			}
+			return
+		}
+		Word(Content, GuildID, discord, ChannelID, Message)
+		return
+	case Prefix(Content, "lleave"):
+		session, err := GetByGuildID(GuildID)
+		if err != nil || session.channelID != ChannelID {
+			if err := discord.MessageReactionAdd(ChannelID, Message, "❌"); err != nil {
+				log.Println(err)
+			}
+			return
+		}
+		Leave(session, discord, ChannelID, Message, true)
+		return
+		//Poll関連
+	case Prefix(Content, "poll "):
+		Poll(Content, Author, discord, ChannelID, Message)
+		return
+	//Role関連
+	case Prefix(Content, "role "):
+		log.Print("role")
+		return
+	//help
+	case Prefix(Content, "help"):
+		Help(discord, ChannelID)
 		return
 	}
 
-	if err = ttsSession.Speech(discord, m.Content); err != nil {
-		log.Println(err)
+	//読み上げ
+	session, err := GetByGuildID(GuildID)
+	if err != nil || session.channelID != ChannelID {
+		return
+	}
+	replace := regexp.MustCompile(*prefix + " once ")
+	text := replace.ReplaceAllString(Content, "")
+	Speech(session, text)
+}
+
+func Prefix(message, check string) bool {
+	return strings.HasPrefix(message, *prefix+" "+check)
+}
+
+func JoinCheck() {
+}
+
+func Join(ChannelID string, GuildID string, discord *discordgo.Session, AuthorID string, Message string) {
+	if voiceConection, err := joinUserVoiceChannel(discord, AuthorID); err != nil {
+		log.Println("missing join vc")
+		if err := discord.MessageReactionAdd(ChannelID, Message, "❌"); err != nil {
+			log.Println(err)
+		}
+		return
+	} else {
+		session := &SessionData{
+			guildID:      GuildID,
+			channelID:    ChannelID,
+			vcsession:    voiceConection,
+			speech_speed: 1.5,
+			speech_limit: 100,
+			speech_lang:  "auto",
+			mut:          sync.Mutex{},
+		}
+		sessions = append(sessions, session)
+		if err := discord.MessageReactionAdd(ChannelID, Message, "✅"); err != nil {
+			log.Println(err)
+		}
+		Speech(session, "おはー")
+		return
 	}
 }
 
-func onVoiceStateUpdate(discord *discordgo.Session, v *discordgo.VoiceStateUpdate) {
-	ttsSession, err := sessionManager.GetByGuidID(v.GuildID)
-	if err == session.ErrTtsSessionNotFound {
+func Speech(session *SessionData, text string) {
+
+	data, _ := os.Open("./dic/" + session.guildID + ".txt")
+	defer data.Close()
+	scanner := bufio.NewScanner(data)
+	for scanner.Scan() {
+		tmp := scanner.Text()
+		replace := regexp.MustCompile(`,.*`)
+		from := replace.ReplaceAllString(tmp, "")
+		replace = regexp.MustCompile(`.*,`)
+		to := replace.ReplaceAllString(tmp, "")
+		replace = regexp.MustCompile(from)
+		text = replace.ReplaceAllString(text, to)
+	}
+
+	if regexp.MustCompile(`<a:|<:|<@|<#|<@&|http|` + "```").MatchString(text) {
+		text = "message skip"
 		return
 	}
+
+	lang := session.speech_lang
+	if lang == "auto" {
+		lang = "ja"
+		if regexp.MustCompile(`^[a-zA-Z0-9\s.,]+$`).MatchString(text) {
+			lang = "en"
+		}
+	}
+
+	//text cut
+	length := len(text)
+	if length > session.speech_limit {
+		text = string([]rune(text)[:session.speech_limit])
+	}
+
+	//改行停止
+	if strings.Contains(text, "\n") {
+		replace := regexp.MustCompile(`\n.*`)
+		text = replace.ReplaceAllString(text, "")
+	}
+
+	//読み上げ待機
+	session.mut.Lock()
+	defer session.mut.Unlock()
+
+	voiceURL := fmt.Sprintf("http://translate.google.com/translate_tts?ie=UTF-8&textlen=32&client=tw-ob&q=%s&tl=%s", url.QueryEscape(text), lang)
+	err := playAudioFile(session, voiceURL)
 	if err != nil {
-		log.Println(err)
+		log.Printf("Error:%s voiceURL:%s", err, voiceURL)
 		return
 	}
+	return
+}
 
-	if ttsSession.VoiceConnection == nil || !ttsSession.VoiceConnection.Ready {
-		return
-	}
+func joinUserVoiceChannel(discord *discordgo.Session, userID string) (*discordgo.VoiceConnection, error) {
+	vs := findUserVoiceState(discord, userID)
+	return discord.ChannelVoiceJoin(vs.GuildID, vs.ChannelID, false, true)
+}
 
-	// ボイスチャンネルに誰かしらいたら return
+func findUserVoiceState(discord *discordgo.Session, userid string) *discordgo.VoiceState {
 	for _, guild := range discord.State.Guilds {
 		for _, vs := range guild.VoiceStates {
-			if ttsSession.VoiceConnection.ChannelID == vs.ChannelID && vs.UserID != clientID {
-				return
+			if vs.UserID == userid {
+				return vs
 			}
 		}
 	}
+	return nil
+}
 
-	// ボイスチャンネルに誰もいなかったら Disconnect する
-	if err := sessionManager.Remove(v.GuildID); err != nil {
-		log.Println(err)
+func playAudioFile(session *SessionData, filename string) error {
+	if err := session.vcsession.Speaking(true); err != nil {
+		return err
 	}
-	err = ttsSession.Leave(discord)
+	defer session.vcsession.Speaking(false)
+
+	opts := dca.StdEncodeOptions
+	opts.CompressionLevel = 0
+	opts.RawOutput = true
+	opts.Bitrate = 120
+	opts.AudioFilter = fmt.Sprintf("atempo=%f", session.speech_speed)
+
+	encodeSession, err := dca.EncodeFile(filename, opts)
 	if err != nil {
+		return err
+	}
+
+	done := make(chan error)
+	stream := dca.NewStream(encodeSession, session.vcsession, done)
+	ticker := time.NewTicker(time.Second)
+
+	for {
+		select {
+		case err := <-done:
+			if err != nil && err != io.EOF {
+				return err
+			}
+			encodeSession.Truncate()
+			return nil
+		case <-ticker.C:
+			playbackPosition := stream.PlaybackPosition()
+			log.Println("Sending Now... : Playback:", playbackPosition)
+		}
+	}
+}
+
+func Speed(session *SessionData, Content string, discord *discordgo.Session, ChannelID string, Message string) {
+	tmp := strings.Replace(Content, *prefix+" sspeed ", "", 1)
+
+	tmp_speed, err := strconv.ParseFloat(tmp, 64)
+	if err != nil {
+		log.Println("missing chenge string to float64")
+		if err := discord.MessageReactionAdd(ChannelID, Message, "❌"); err != nil {
+			log.Println(err)
+		}
+		return
+	}
+
+	if tmp_speed < 0.5 || 100 < tmp_speed {
+		log.Println("missing lowest or highest speed")
+		if err := discord.MessageReactionAdd(ChannelID, Message, "❌"); err != nil {
+			log.Println(err)
+		}
+		return
+	}
+
+	session.speech_speed = tmp_speed
+	if err := discord.MessageReactionAdd(ChannelID, Message, "🔊"); err != nil {
+		log.Println(err)
+	}
+	return
+}
+
+func Lang(session *SessionData, Content string, discord *discordgo.Session, ChannelID string, Message string) {
+	tmp := strings.Replace(Content, *prefix+" llang ", "", 1)
+
+	if tmp == "auto" {
+		session.speech_lang = "auto"
+		return
+	}
+
+	_, err := language.Parse(tmp)
+	if err != nil {
+		log.Println("missing chenge to unknown Language")
+		if err := discord.MessageReactionAdd(ChannelID, Message, "❌"); err != nil {
+			log.Println(err)
+		}
+		return
+	}
+
+	session.speech_lang = tmp
+	if err := discord.MessageReactionAdd(ChannelID, Message, "🗣️"); err != nil {
+		log.Println(err)
+	}
+	return
+}
+
+func Limit(session *SessionData, Content string, discord *discordgo.Session, ChannelID string, Message string) {
+	tmp := strings.Replace(Content, *prefix+" llimit ", "", 1)
+
+	tmp_limit, err := strconv.Atoi(tmp)
+	if err != nil {
+		log.Println("missing chenge string to int")
+		if err := discord.MessageReactionAdd(ChannelID, Message, "❌"); err != nil {
+			log.Println(err)
+		}
+		return
+	}
+
+	if tmp_limit <= 0 {
+		log.Println("missing lowest limit")
+		if err := discord.MessageReactionAdd(ChannelID, Message, "❌"); err != nil {
+			log.Println(err)
+		}
+		return
+	}
+	session.speech_limit = tmp_limit
+	if err := discord.MessageReactionAdd(ChannelID, Message, "🥺"); err != nil {
+		log.Println(err)
+	}
+	return
+}
+
+func Word(Content string, GuildID string, discord *discordgo.Session, ChannelID string, Message string) {
+	tmp := strings.Replace(Content, *prefix+" wword ", "", 1)
+
+	if file, err := os.OpenFile("./dic/"+GuildID+".txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0777); err != nil {
+		//エラー処理
+		log.Println("missing writing")
+		if err := discord.MessageReactionAdd(ChannelID, Message, "❌"); err != nil {
+			log.Println(err)
+		}
+		return
+	} else {
+		defer file.Close()
+		fmt.Fprintln(file, tmp)
+		if err := discord.MessageReactionAdd(ChannelID, Message, "📄"); err != nil {
+			log.Println(err)
+		}
+		return
+	}
+}
+
+func Leave(session *SessionData, discord *discordgo.Session, ChannelID string, Message string, Reaction bool) {
+	Speech(session, "さいなら")
+
+	if err := session.vcsession.Disconnect(); err != nil {
+		log.Println("missing disconnect")
+		if Reaction {
+			if err := discord.MessageReactionAdd(ChannelID, Message, "❌"); err != nil {
+				log.Println(err)
+			}
+		}
+		return
+	} else {
+		var ret []*SessionData
+		for _, v := range sessions {
+			if v.guildID == session.guildID {
+				continue
+			}
+			ret = append(ret, v)
+		}
+		sessions = ret
+		if Reaction {
+			if err := discord.MessageReactionAdd(ChannelID, Message, "⛔"); err != nil {
+				log.Println(err)
+			}
+		}
+		return
+	}
+}
+
+func Poll(Content string, Author string, discord *discordgo.Session, ChannelID string, Message string) {
+	//長さ確認
+	replace := regexp.MustCompile(*prefix + " poll|,$")
+	poll := replace.ReplaceAllString(Content, "")
+	text := strings.Split(poll, ",")
+	//Title+Questionだから-1
+	length := len(text) - 1
+	if length <= 20 {
+		//embedとかreaction用のやつ
+		alphabet := []string{"", "🇦", "🇧", "🇨", "🇩", "🇪", "🇫", "🇬", "🇭", "🇮", "🇯", "🇰", "🇱", "🇲", "🇳", "🇴", "🇵", "🇶", "🇷", "🇸", "🇹"}
+		//embedのtmp作成
+		embed := &discordgo.MessageEmbed{
+			Type:        "rich",
+			Title:       "",
+			Description: "",
+			Color:       1000,
+			Author:      &discordgo.MessageEmbedAuthor{Name: ""},
+		}
+		//作成者表示
+		embed.Author.Name = "create by @" + Author
+		//Titleの設定
+		embed.Title = text[0]
+		//中身の設定
+		Question := ""
+		for i := 1; i < len(text); i++ {
+			Question = Question + alphabet[i] + " : " + text[i] + "\n"
+		}
+		embed.Description = Question
+		//送信
+		message, err := discord.ChannelMessageSendEmbed(ChannelID, embed)
+		if err != nil {
+			log.Println(err)
+		}
+		//リアクションと中身の設定
+		for i := 1; i < len(text); i++ {
+			Question = Question + alphabet[i] + text[i] + "\n"
+			if err := discord.MessageReactionAdd(ChannelID, message.ID, alphabet[i]); err != nil {
+				log.Println(err)
+			}
+		}
+	} else {
+		if err := discord.MessageReactionAdd(ChannelID, Message, "❌"); err != nil {
+			log.Println(err)
+		}
+	}
+}
+
+func Help(discord *discordgo.Session, ChannelID string) {
+	//embedのtmp作成
+	embed := &discordgo.MessageEmbed{
+		Type:        "rich",
+		Title:       "BOT HELP",
+		Description: "",
+		Color:       1000,
+	}
+	Text := "--TTS--\n" +
+		*prefix + " join :VCに参加します\n" +
+		*prefix + " speed <速度> : 読み上げ速度を変更します\n" +
+		*prefix + " lang <言語> : 読み上げ言語を変更します\n" +
+		*prefix + " limit <文字数> : 読み上げ文字数の上限を設定します\n" +
+		*prefix + " leave : VCから切断します\n" +
+		"--Poll--\n" +
+		*prefix + " poll <質問>,<回答1>,<回答2>... : 質問を作成します\n"
+	embed.Description = Text
+	//送信
+	if _, err := discord.ChannelMessageSendEmbed(ChannelID, embed); err != nil {
 		log.Println(err)
 	}
 }
 
-func PrefixCheck(message, command string) bool {
-	return strings.HasPrefix(message, *prefix+" "+command)
-}
+//VCでJoin||Leaveが起きたときにCall
+func onVoiceStateUpdate(discord *discordgo.Session, v *discordgo.VoiceStateUpdate) {
 
-func sendMessage(discord *discordgo.Session, textChanelID, format string, v ...interface{}) {
-	session := session.NewTtsSession()
-	session.TextChanelID = textChanelID
-	session.SendMessage(discord, format, v...)
+	tmp := ","
+	for _, guild := range discord.State.Guilds {
+		for _, vs := range guild.VoiceStates {
+			if v.ChannelID == vs.ChannelID && vs.UserID != clientID {
+				return
+			}
+			tmp = tmp + vs.UserID + ","
+		}
+	}
+	if strings.Count(tmp, ",") == 2 {
+		session, err := GetByGuildID(v.GuildID)
+		if err != nil {
+			return
+		}
+		Leave(session, discord, "", "", false)
+	}
 }
