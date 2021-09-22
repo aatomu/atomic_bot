fd35030764ed88046b93091de4f9cdfc8932979e
atomu21263 <atom.aizu@gmail.com>
2021-09-22 19:34:51 +0900

    typo

--- main.go
+++ main.go
@@ -228,15 +228,15 @@ func onMessageCreate(discord *discordgo.Session, m *discordgo.MessageCreate) {
 	}
 	filesURL := ""
 	if len(m.Attachments) > 0 {
-		filesURL := "Files: \""
+		filesURL = "Files: \""
 		for _, file := range m.Attachments {
-			filesURL = filesURL + file.URL
+			filesURL = filesURL + file.URL + ","
 		}
-		filesURL = filesURL + "\""
+		filesURL = filesURL + "\"  "
 	}
 
 	//表示
-	log.Print("Guild:\"" + guildName + "\"  Channel:\"" + channel.Name + "\"  " + filesURL + "  " + author + ": " + message)
+	log.Print("Guild:\"" + guildName + "\"  Channel:\"" + channel.Name + "\"  " + filesURL + author + ": " + message)
 
 	//bot 読み上げ無し のチェック
 	if m.Author.Bot || strings.HasPrefix(m.Content, ";") {
